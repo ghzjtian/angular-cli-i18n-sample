927db3520d372638b113aecdf22729cbc8e5282e
Tim <wyzjtian@163.com>
2019-08-13 17:41:39 +0800

    1.Add locale string log.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, LOCALE_ID, Inject} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cli-i18n-sample';
+    title = 'angular-cli-i18n-sample';
+    myLocaleId: string;
+    languages = [
+        {code: 'en', label: 'English'},
+        {code: 'zh', label: '中文'}
+    ];
+
+    constructor(@Inject(LOCALE_ID) protected localeId: string) {
+        console.log(`localeId:${this.localeId}`);
+        console.log(`constructor`);
+        this.myLocaleId = this.localeId ;
+    }
+
 }
